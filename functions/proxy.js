0d0ea0a8b7f063c810196e672f9cd4bc3d307c1d
unknowncyber256 <166958496+unknowncyber256@users.noreply.github.com>
2026-06-13 09:24:04 +0300

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,40 +1,10 @@
 exports.handler = async (event) => {
-  const { httpMethod, path, headers, body } = event;
-
-  // Handle CORS preflight
-  if (httpMethod === 'OPTIONS') {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': '*',
-      },
-      body: '',
-    };
-  }
-
-  // Determine target host (can be overridden by x-target-host header, default to munowatch.org)
-  let targetHost = headers['x-target-host'];
-  if (!targetHost) targetHost = 'munowatch.org';
-
-  // Build the full target URL
-  const targetUrl = `https://${targetHost}${path}`;
-
-  // Prepare headers to forward (remove conflicting ones)
-  const forwardHeaders = { ...headers };
-  delete forwardHeaders['host'];
-  delete forwardHeaders['x-target-host'];
-  delete forwardHeaders['content-length'];
-
-  const fetchOptions = {
-    method: httpMethod,
-    headers: forwardHeaders,
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message: 'Proxy function is working!' }),
   };
-  if (body) fetchOptions.body = body;
-
-  try {
-    const upstream = await fetch(targetUrl, fetchOptions);
+};    const upstream = await fetch(targetUrl, fetchOptions);
     const responseBody = await upstream.text();
 
     const responseHeaders = {
